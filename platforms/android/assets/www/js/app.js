e8f93f6520eec6dc1b82beef56b001286769b81b
johnnynode <johnnynode@gmail.com>
2017-10-31 17:11:53 +0800

    remove .gitignore platform and plugins

---
+++ platforms/android/assets/www/js/app.js
@@ -0,0 +1,113 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+    // 隐藏启动页
+    if (navigator.splashscreen) {
+      navigator.splashscreen.hide();
+    }
+
+  });
+})
+
+.config(function($urlRouterProvider, $httpProvider, $ionicConfigProvider) {
+    $urlRouterProvider.otherwise('/tab/dash'); // 默认路由
+    // $httpProvider.interceptors.push('authInterceptor'); // 设置拦截器
+    ionic.Platform.isFullScreen = false; // 禁止全屏显示
+
+    $ionicConfigProvider.views.maxCache(15);
+    $ionicConfigProvider.views.transition('platform');
+    $ionicConfigProvider.views.forwardCache(true); // 缓存下一页
+    $ionicConfigProvider.views.swipeBackEnabled(ionic.Platform.isIOS());
+    $ionicConfigProvider.spinner.icon('ios');
+
+    // 通用样式的兼容
+    $ionicConfigProvider.platform.android.tabs.position("bottom");
+    $ionicConfigProvider.platform.ios.tabs.position("bottom");
+    $ionicConfigProvider.platform.ios.navBar.alignTitle('center');
+    $ionicConfigProvider.platform.android.navBar.alignTitle('center');
+    $ionicConfigProvider.platform.ios.backButton.previousTitleText('').icon('ion-ios-arrow-left');
+    $ionicConfigProvider.platform.android.backButton.previousTitleText('').icon('ion-ios-arrow-left');
+    $ionicConfigProvider.tabs.style('standard');
+    // false 默认所有的滚动使用native，会比js的滚动快很多，并且很平滑 ; 安卓使用,ios不使用
+    $ionicConfigProvider.scrolling.jsScrolling(!ionic.Platform.isAndroid());
+  })
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+  // setup an abstract state for the tabs directive
+    .state('tab', {
+    url: '/tab',
+    abstract: true,
+    templateUrl: 'templates/tabs.html'
+  })
+
+  // Each tab has its own nav history stack:
+
+  .state('tab.dash', {
+    url: '/dash',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/tab-dash.html',
+        controller: 'DashCtrl'
+      }
+    }
+  })
+
+  .state('tab.chats', {
+      url: '/chats',
+      views: {
+        'tab-chats': {
+          templateUrl: 'templates/tab-chats.html',
+          controller: 'ChatsCtrl'
+        }
+      }
+    })
+    .state('tab.chat-detail', {
+      url: '/chats/:chatId',
+      views: {
+        'tab-chats': {
+          templateUrl: 'templates/chat-detail.html',
+          controller: 'ChatDetailCtrl'
+        }
+      }
+    })
+
+  .state('tab.account', {
+    url: '/account',
+    views: {
+      'tab-account': {
+        templateUrl: 'templates/tab-account.html',
+        controller: 'AccountCtrl'
+      }
+    }
+  });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/tab/dash');
+
+});
